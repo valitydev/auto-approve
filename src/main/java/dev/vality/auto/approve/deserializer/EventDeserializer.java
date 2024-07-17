2c1ecfd9cd6d904ce259e3e8a2a742bd8cdb7de7
ggmaleva <ggmaleva@yandex.ru>
2024-07-17 15:41:04 +0300

    add log

--- src/main/java/dev/vality/auto/approve/deserializer/EventDeserializer.java
+++ src/main/java/dev/vality/auto/approve/deserializer/EventDeserializer.java
@@ -5,13 +5,16 @@ import dev.vality.damsel.claim_management.ClaimUpdated;
 import dev.vality.damsel.claim_management.Event;
 import dev.vality.kafka.common.exception.KafkaSerializationException;
 import dev.vality.kafka.common.serialization.AbstractThriftDeserializer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EventDeserializer extends AbstractThriftDeserializer<Event> {
 
     public Event deserialize(String s, byte[] bytes) {
         try {
             return super.deserialize(bytes, new Event());
         } catch (KafkaSerializationException e) {
+            log.info("Ignore event with deserialization error");
             Event event = new Event();
             event.setChange(Change.updated(new ClaimUpdated()));
             return event;
