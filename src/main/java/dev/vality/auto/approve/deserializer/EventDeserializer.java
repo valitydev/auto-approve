f37387fd881738d2a411b98a9cd0d5924e888ebf
ggmaleva <ggmaleva@yandex.ru>
2024-07-17 15:16:50 +0300

    ignore deserialization error

--- src/main/java/dev/vality/auto/approve/deserializer/EventDeserializer.java
+++ src/main/java/dev/vality/auto/approve/deserializer/EventDeserializer.java
@@ -1,12 +1,21 @@
 package dev.vality.auto.approve.deserializer;
 
+import dev.vality.damsel.claim_management.Change;
+import dev.vality.damsel.claim_management.ClaimUpdated;
 import dev.vality.damsel.claim_management.Event;
+import dev.vality.kafka.common.exception.KafkaSerializationException;
 import dev.vality.kafka.common.serialization.AbstractThriftDeserializer;
 
 public class EventDeserializer extends AbstractThriftDeserializer<Event> {
 
     public Event deserialize(String s, byte[] bytes) {
-        return super.deserialize(bytes, new Event());
+        try {
+            return super.deserialize(bytes, new Event());
+        } catch (KafkaSerializationException e) {
+            Event event = new Event();
+            event.setChange(Change.updated(new ClaimUpdated()));
+            return event;
+        }
     }
 
 }
